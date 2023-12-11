21881ec74b9bb838ea661fe552837cbe9a51f1a0
Sato <lutereau.s@gmail.com>
2023-12-11 20:43:36 -0300

    added panel

---
+++ front/src/Views/Panel/Panel.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react'
+
+function Panel() {
+
+  useEffect(()=>{
+
+  },[])
+
+  return (
+    <div className="panel-container">
+      <div>
+        <h1>Panel</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Panel
