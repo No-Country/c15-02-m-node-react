dec4e5364b2923a90deacf8ffd60631b9e73579b
Sato <lutereau.s@gmail.com>
2023-12-11 21:11:27 -0300

    token status endpoint

--- back/src/routes/auth.routes.js
+++ back/src/routes/auth.routes.js
@@ -1,10 +1,20 @@
-const express = require('express')
-const { authController, userController } = require('../controllers/index.controller')
-const { validateLogin, validateRegistration } = require('../middleware/validations/loginValidation.mdw')
-const authRouter = express.Router()
+const express = require("express");
+const {
+  authController,
+  userController,
+} = require("../controllers/index.controller");
+const {
+  validateLogin,
+  validateRegistration,
+} = require("../middleware/validations/loginValidation.mdw");
+const { authenticateToken } = require("../middleware/auth.mdw");
+const HttpResponse = require("../handlers/HttpResponse");
+const authRouter = express.Router();
 
 
-authRouter.post('/login', validateLogin, authController.validateUser)
-authRouter.post('/register', validateRegistration, userController.createUser)
-
-module.exports = authRouter
+authRouter.post("/login", validateLogin, authController.validateUser);
+authRouter.post("/register", validateRegistration, userController.createUser);
+authRouter.post("/token", authenticateToken, (req, res) => {
+  return HttpResponse.noContent(res);
+});
+module.exports = authRouter;
